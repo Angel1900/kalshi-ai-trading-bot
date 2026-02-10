d602047aff604d3608e25af0ad16a854bcfa72e6
Angel1900 <34906887+Angel1900@users.noreply.github.com>
2026-02-09 16:56:17 -0800

    Add src package init

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+"""Kalshi AI Trading Bot - Main Package"""
+
+__version__ = '1.0.0'
+__author__ = 'Angel1900'
+__description__ = 'Autonomous prediction market trading system for Kalshi'
+
+from src.config import Config
+
+__all__ = ['Config']
